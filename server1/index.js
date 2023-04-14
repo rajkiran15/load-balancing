5ff76dfd4fe205ace2bdd1dffecd74a91054111f
rajkiran15 <57134027+rajkiran15@users.noreply.github.com>
2023-04-14 15:43:21 +0530

    minor changes

---
+++ server1/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const PORT = 3001;
+
+app.get("/",(req,res)=>{
+    res.send("hii how are you"+PORT);
+})
+
+app.listen(PORT,() => {
+    console.log('server running on port '+" " +PORT);
+})
